4b1a31eee1aee269a5334216364733204ea6c060
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2018-10-22 23:28:51 -0700

    Add Python 3 support to nsmblib (and change its version to 0.4.1)

--- nsmblib-src/nsmblibmodule.c
+++ nsmblib-src/nsmblibmodule.c
@@ -1,6 +1,5 @@
 #include <Python.h>
-#include "list.h"
-
+#include "bytesobject.h"
 #define s32 signed int
 #define s16 signed short
 #define s8 signed char
@@ -25,11 +24,10 @@
  * 0.2: Added nsmblib_decompress11LZS
  * 0.3: Fixed hosed memory addressing in nsmblib_decompress11LZS
  * 0.4: Added nsmblib_getVersion
- * 0.5: Added nsmblib_compress11LZS (thanks to puyo_tools
- *      for the original C# code)
+ * 0.4.1: Added Python 3 compatibility
  */
 
-#define CURRENT_VERSION 5
+#define CURRENT_VERSION 4
 
 static PyObject *nsmblib_getVersion(PyObject *self, PyObject *args) {
     /* Gets the current version of the NSMB module.
@@ -47,7 +45,7 @@ static PyObject *nsmblib_decompress11LZS(PyObject *self, PyObject *args) {
      *  - str data (containing the compressed data)
      */
 
-    const u8 *data;
+    const char *data;
     int datalength;
 
     u8 *decoded;
@@ -56,7 +54,7 @@ static PyObject *nsmblib_decompress11LZS(PyObject *self, PyObject *args) {
 
     /* used while decompressing */
     int curr_size;
-    const u8 *source;
+    const char *source;
     u8 *dest, *end;
     int len, i, j, cdest, disp, flag;
     u8 b1, b2, b3, bt, flags;
@@ -173,261 +171,8 @@ static PyObject *nsmblib_decompress11LZS(PyObject *self, PyObject *args) {
     }
 
     /* return it */
-    retvalue = PyString_FromStringAndSize((const char*)decoded, decompsize);
+    retvalue = PyBytes_FromStringAndSize((const char*)decoded, decompsize);
     PyMem_Free(decoded);
-
-    return retvalue;
-}
-
-typedef struct LZDict_t {
-    int WindowSize;
-    int WindowStart;
-    int WindowLength;
-    int MinMatchAmount;
-    int MaxMatchAmount;
-    int BlockSize;
-    IntList OffsetList[0x100];
-} LZDict;
-
-void LZDict_init(LZDict *dict);
-void LZDict_free(LZDict *dict);
-void LZDict_search(LZDict *dict, u8 *data, int offset, int length, int *ret1, int *ret2);
-void LZDict_slide_window(LZDict *dict, int amount);
-void LZDict_slide_block(LZDict *dict);
-void LZDict_remove_old_entries(LZDict *dict, u8 index);
-void LZDict_set_window_size(LZDict *dict, int size);
-void LZDict_set_min_match_amount(LZDict *dict, int amount);
-void LZDict_set_max_match_amount(LZDict *dict, int amount);
-void LZDict_set_block_size(LZDict *dict, int size);
-void LZDict_add_entry(LZDict *dict, u8 *data, int offset);
-void LZDict_add_entry_range(LZDict *dict, u8 *data, int offset, int length);
-
-
-void LZDict_init(LZDict *dict) {
-    /* set up the dict */
-    int i;
-    for (i = 0; i < 0x100; i++) {
-        IntList_init(&(dict->OffsetList[i]));
-    }
-
-    dict->WindowSize = 0x1000;
-    dict->WindowStart = 0;
-    dict->WindowLength = 0;
-    dict->MinMatchAmount = 3;
-    dict->MaxMatchAmount = 18;
-    dict->BlockSize = 0;
-}
-
-void LZDict_free(LZDict *dict) {
-    /* free the dict */
-    int i;
-    for (i = 0; i < 0x100; i++) {
-        IntList_free(&(dict->OffsetList[i]));
-    }
-}
-
-void LZDict_search(LZDict *dict, u8 *data, int offset, int length, int *ret1, int *ret2) {
-    int i;
-    int MatchStart;
-    int MatchSize;
-
-    LZDict_remove_old_entries(dict, data[offset]);
-
-    if (offset < dict->MinMatchAmount || length - offset < dict->MinMatchAmount) {
-        *ret1 = 0;
-        *ret2 = 0;
-        return;
-    }
-
-    // start finding matches
-    *ret1 = 0;
-    *ret2 = 0;
-
-    for (i = dict->OffsetList[data[offset]].Count - 1; i >= 0; i--) {
-        MatchStart = dict->OffsetList[data[offset]].Elements[i];
-        MatchSize = 1;
-
-        while (MatchSize < dict->MaxMatchAmount && MatchSize < dict->WindowLength && MatchStart + MatchSize < offset && offset + MatchSize < length && data[offset + MatchSize] == data[MatchStart + MatchSize])
-            MatchSize++;
-
-        if (MatchSize >= dict->MinMatchAmount && MatchSize > *ret2) {
-            // this is a good match
-            *ret1 = offset - MatchStart;
-            *ret2 = MatchSize;
-            if (MatchSize == dict->MaxMatchAmount)
-                break; // don't look for more matches
-        }
-    }
-}
-
-void LZDict_slide_window(LZDict *dict, int amount) {
-    if (dict->WindowLength == dict->WindowSize)
-        dict->WindowStart += amount;
-    else {
-        if (dict->WindowLength + amount <= dict->WindowSize)
-            dict->WindowLength += amount;
-        else {
-            amount -= (dict->WindowSize - dict->WindowLength);
-            dict->WindowLength = dict->WindowSize;
-            dict->WindowStart += amount;
-        }
-    }
-}
-
-void LZDict_slide_block(LZDict *dict) {
-    dict->WindowStart += dict->BlockSize;
-}
-
-void LZDict_remove_old_entries(LZDict *dict, u8 index) {
-    int i;
-
-    for (i = 0; i < dict->OffsetList[index].Count;) { // don't increment i
-        if (dict->OffsetList[index].Elements[i] >= dict->WindowStart)
-            break;
-        else
-            IntList_removeAt(&(dict->OffsetList[index]), 0);
-    }
-}
-
-void LZDict_set_window_size(LZDict *dict, int size) {
-    dict->WindowSize = size;
-}
-
-void LZDict_set_min_match_amount(LZDict *dict, int amount) {
-    dict->MinMatchAmount = amount;
-}
-
-void LZDict_set_max_match_amount(LZDict *dict, int amount) {
-    dict->MaxMatchAmount = amount;
-}
-
-void LZDict_set_block_size(LZDict *dict, int size) {
-    dict->BlockSize = size;
-    dict->WindowLength = size;
-}
-
-void LZDict_add_entry(LZDict *dict, u8 *data, int offset) {
-    IntList_add(&(dict->OffsetList[data[offset]]), offset);
-}
-
-void LZDict_add_entry_range(LZDict *dict, u8 *data, int offset, int length) {
-    int i;
-    for (i = 0; i < length; i++)
-        IntList_add(&(dict->OffsetList[data[offset]]), offset + i);
-}
-
-static PyObject *nsmblib_compress11LZS(PyObject *self, PyObject *args) {
-    /* Compresses a file using LZSS 0x11 variant.
-     * Returns: str (containing the compressed data)
-     * Parameters:
-     *  - str data (containing the decompressed data)
-     */
-
-    const u8 *data;
-    int datalength;
-
-    u8 *src_ptr;
-    u8 *dest_ptr;
-
-    u8 *end_src_ptr;
-
-    u8 *buffer;
-    int bufSize;
-
-    PyObject *retvalue;
-    int i, ret1, ret2; // used in the compression loop
-    LZDict dict;
-
-    /* get the arguments */
-    if (!PyArg_ParseTuple(args, "s#", &data, &datalength))
-        return NULL;
-
-    /* allocate a buffer to start with */
-    bufSize = datalength;
-    buffer = (char*)PyMem_Malloc(bufSize);
-    if (buffer == NULL)
-        return PyErr_NoMemory();
-
-    src_ptr = (u8*)data;
-    dest_ptr = buffer;
-
-    end_src_ptr = (u8*)data + datalength;
-
-    LZDict_init(&dict);
-    LZDict_set_window_size(&dict, 0x1000);
-    LZDict_set_max_match_amount(&dict, 0xFFFF + 273);
-
-    /* write the decomp size */
-    if (datalength <= 0xFFFFFF) {
-        *dest_ptr++ = 0x11;
-        *dest_ptr++ = (datalength & 0xFF);
-        *dest_ptr++ = ((datalength >> 8) & 0xFF);
-        *dest_ptr++ = ((datalength >> 16) & 0xFF);
-    } else {
-        *dest_ptr++ = 0x11;
-        *dest_ptr++ = 0x00;
-        *dest_ptr++ = 0x00;
-        *dest_ptr++ = 0x00;
-        *dest_ptr++ = (datalength & 0xFF);
-        *dest_ptr++ = ((datalength >> 8) & 0xFF);
-        *dest_ptr++ = ((datalength >> 16) & 0xFF);
-        *dest_ptr++ = ((datalength >> 24) & 0xFF);
-    }
-
-    /* start compression */
-    while (src_ptr < end_src_ptr) {
-        u8 flag = 0;
-        u8 *flagpos = dest_ptr;
-        *dest_ptr++ = flag;
-
-        for (i = 7; i >= 0; i--) {
-            LZDict_search(&dict, data, src_ptr - data, datalength, &ret1, &ret2);
-            if (ret2 > 0) { /* there is a compression match */
-                flag |= (u8)(1 << i);
-
-                /* write the distance/length pair */
-                if (ret2 <= 0xF + 1) { /* 2 bytes */
-                    *dest_ptr++ = (((ret2 - 1) & 0xF) << 4) | (((ret1 - 1) & 0xFFF) >> 8);
-                    *dest_ptr++ = ((ret1 - 1) & 0xFF);
-                } else if (ret2 <= 0xFF + 17) { /* 3 bytes */
-                    *dest_ptr++ = (((ret2 - 17) & 0xFF) >> 4);
-                    *dest_ptr++ = ((((ret2 - 17) & 0xF) << 4) | (((ret1 - 1) & 0xFFF) >> 8));
-                    *dest_ptr++ = ((ret1 - 1) & 0xFF);
-                } else { /* 4 bytes */
-                    *dest_ptr++ = ((1 << 4) | (((ret2 - 273) & 0xFFFF) >> 12));
-                    *dest_ptr++ = (((ret2 - 273) & 0xFFF) >> 4);
-                    *dest_ptr++ = ((((ret2 - 273) & 0xF) << 4) | (((ret1 - 1) & 0xFFF) >> 8));
-                    *dest_ptr++ = ((ret1 - 1) & 0xFF);
-                }
-
-                LZDict_add_entry_range(&dict, data, src_ptr - data, ret2);
-                LZDict_slide_window(&dict, ret2);
-
-                src_ptr += ret2;
-
-            } else { /* there wasn't a match */
-                flag |= (u8)(0 << i);
-
-                LZDict_add_entry(&dict, data, src_ptr - data);
-                LZDict_slide_window(&dict, 1);
-
-                *dest_ptr++ = *src_ptr++;
-            }
-
-            /* check for out of bounds */
-            if (src_ptr >= end_src_ptr)
-                break;
-        }
-
-        /* write the flag */
-        *flagpos = flag;
-    }
-
-    LZDict_free(&dict);
-
-    /* return it! */
-    retvalue = PyString_FromStringAndSize((const char*)buffer, dest_ptr - buffer);
-    PyMem_Free(buffer);
 
     return retvalue;
 }
@@ -522,7 +267,7 @@ static PyObject *nsmblib_decodeTileset(PyObject *self, PyObject *args) {
     }
 
     /* return it */
-    retvalue = PyString_FromStringAndSize(decoded, 1048576);
+    retvalue = PyBytes_FromStringAndSize(decoded, 1048576);
     PyMem_Free(decoded);
 
     return retvalue;
@@ -533,14 +278,55 @@ static PyMethodDef NSMBLibMethods[] = {
      "Gets the current version of the NSMB module."},
     {"decompress11LZS", nsmblib_decompress11LZS, METH_VARARGS,
      "Decompresses a file using LZSS 0x11 variant."},
-    {"compress11LZS", nsmblib_compress11LZS, METH_VARARGS,
-     "Compresses a file using LZSS 0x11 variant."},
     {"decodeTileset", nsmblib_decodeTileset, METH_VARARGS,
      "Decodes an uncompressed RGB5A4 tileset into ARGB32 Premultiplied."},
     {NULL, NULL, 0, NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
+
+struct module_state {
+    PyObject *error;
+};
+#define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
+static int nsmblib_traverse(PyObject *m, visitproc visit, void *arg) {
+    Py_VISIT(GETSTATE(m)->error);
+    return 0;
+}
+static int nsmblib_clear(PyObject *m) {
+    Py_CLEAR(GETSTATE(m)->error);
+    return 0;
+}
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "nsmblib",
+        NULL,
+        sizeof(struct module_state),
+        NSMBLibMethods,
+        NULL,
+        nsmblib_traverse,
+        nsmblib_clear,
+        NULL
+};
+#endif
+
+#if PY_MAJOR_VERSION >= 3
 PyMODINIT_FUNC
-initnsmblib(void) {
-    (void)Py_InitModule("nsmblib", NSMBLibMethods);
+PyInit_nsmblib(void)
+#else
+void
+initnsmblib(void)
+#endif
+{
+
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&moduledef);
+#else
+    PyObject *module = Py_InitModule("nsmblib", NSMBLibMethods);
+#endif
+
+#if PY_MAJOR_VERSION >= 3
+    return module;
+#endif
 }
