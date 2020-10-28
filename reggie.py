bde96ccd448e3944531038deb99eec9370c01570
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2020-10-28 14:17:17 -0400

    Fix level-info parsing on Py3/PyQt5
    Many existing levels have PyQt4.QtCore.QStrings embedded directly into the level info pickle data. This change enables Reggie to parse such data, even on PyQt5.

--- reggie.py
+++ reggie.py
@@ -25,6 +25,7 @@ import encodings # fixes "LookupError: no codec search functions
                  # Py2+cx_Freeze+Linux
 import os.path
 import pickle
+import pickletools
 import struct
 import sys
 import time
@@ -567,6 +568,57 @@ def LoadEntranceNames():
 
     with open('reggiedata/entrancetypes.txt', 'r') as getit:
         EntranceTypeNames = [x.strip() for x in getit.readlines()]
+
+
+def DecodeReggieInfo(data, validKeys):
+    """
+    Decode the provided level info data into a dictionary, which will
+    have only the keys specified. Raises an exception if the data can't
+    be parsed.
+    """
+    # The idea here is that we implement just enough of the pickle
+    # protocol (v2) to be able to parse the dictionaries that past
+    # Reggies have pickled, even if PyQt4 isn't available.
+    #
+    # We keep track of the stack and memo, just enough to figure out
+    # in what order the strings are pushed to the stack. (We need to
+    # implement the memo because default level info uses memoization to
+    # avoid encoding the '-' string more than once.) Then we filter out
+    # 'PyQt4.QtCore' and 'QString'. Assuming nobody's crazy enough to
+    # use those as actual level info field values, that should leave us
+    # with exactly 12 strings (6 field names and 6 fields). Then we just
+    # put the dictionary together in the same way as the SETITEMS pickle
+    # instruction, and we're done.
+
+    # Figure out in what order strings are pushed to the pickle stack
+    stack = []
+    memo = {}
+    for inst, arg, _ in pickletools.genops(data):
+        if inst.name in ['SHORT_BINSTRING', 'BINSTRING', 'BINUNICODE']:
+            stack.append(arg)
+        elif inst.name == 'GLOBAL':
+            # In practice, this is used to push sip._unpickle_type,
+            # which then gets BINGET'd over and over. So we have to take
+            # it into account, or else we get confused and end up
+            # pushing some random string to the stack repeatedly instead
+            stack.append(None)
+        elif inst.name == 'BINPUT' and stack:
+            memo[arg] = stack[-1]
+        elif inst.name == 'BINGET' and arg in memo:
+            stack.append(memo[arg])
+
+    # Filter out uninteresting strings and check that the length is right
+    strings = [s for s in stack if s not in {'PyQt4.QtCore', 'QString', None}]
+    assert len(strings) == 12
+
+    # Convert e.g. [a, b, c, d, e, f] -> {a: b, c: d, e: f}
+    # https://stackoverflow.com/a/12739974
+    it = iter(strings)
+    levelinfo = dict(zip(it, it))
+
+    # Double-check that the keys are as expected, and return
+    assert set(levelinfo) == validKeys
+    return levelinfo
 
 
 class ChooseLevelNameDialog(QtWidgets.QDialog):
@@ -2066,9 +2118,7 @@ class LevelUnit():
 
         if data is not None:
             try:
-                level_info = pickle.loads(data)
-                for k,v in level_info.items():
-                    if k in info: info[k] = v
+                info = DecodeReggieInfo(data, set(info.keys()))
             except Exception:
                 pass
 
