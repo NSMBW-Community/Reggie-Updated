ca130d5e3b822be85870f778e16b48d7c24bb881
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2021-03-09 21:24:14 -0600

    Version QSettings on bindings library
    PySide and PyQt QSettings aren't really compatible with each other. Nor are PyQt5 and PyQt6, in some cases. So separating all of these into separate settings should improve reliability... at the cost of actually sharing settings across bindings libraries, unfortunately.

--- reggie.py
+++ reggie.py
@@ -9564,7 +9564,7 @@ def main():
     sprites.Setup()
 
     # load the settings
-    settings = QtCore.QSettings('Reggie', 'Reggie Level Editor')
+    settings = QtCore.QSettings('Reggie', 'Reggie Level Editor (%s)' % QtName)
 
     if '-clear-settings' in sys.argv:
         settings.clear()
